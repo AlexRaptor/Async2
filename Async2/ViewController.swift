77fef9e7e41353ac31b7bcb0f62ca4754fad40c5
STUDENT <student@macbook1sh3qd.local>
2019-04-27 10:47:41 +0700

    Initial Commit

---
+++ Async2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Async2
+//
+//  Created by STUDENT on 27/04/2019.
+//  Copyright © 2019 STUDENT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
